dd918093ad0fa41c4057f03effc331087c8b0928
RainFlavell <apple@Apples-MacBook.gateway>
2016-05-31 18:06:20 +1000

    does it work?

---
+++ project/content/actors/CarActor.js
@@ -0,0 +1,20 @@
+var newActorClass = {};
+//SpawnActor({location: {x:0,y:0}, rotation: 0, size:{x:164,y:124}}, "playerActor")
+
+newActorClass.tickEvent = function(dt, id){
+  //console.log("playerActor Got a tick :D");
+}
+
+newActorClass.EventPlay = function(id){
+  //I am controlled by GetActorById(id).controlledActorID
+  //Getting a controller input value  GetControllerById(GetActorById(id).controlledActorID).axis["MoveForward"]
+  console.info("Spawned Actor: ", GetActorById(id));
+}
+
+newActorClass.onPossess = function(myid, controllerid){
+  //console.log("I've been possesed")
+}
+
+newActorClass.unPossess = function(myid, controllerid){
+  //console.log("I've been unpossesed")
+}
