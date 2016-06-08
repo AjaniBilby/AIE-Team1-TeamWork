92733fe987528079babaee113b1144f95c30f788
Hobgoblin101 <aj@bilby.net>
2016-06-08 19:55:51 +1000

    Fixed distanceTo, rotateVector, lookAt functions

--- project/content/actors/AnimationTestActor.js
+++ project/content/actors/AnimationTestActor.js
@@ -2,12 +2,11 @@ var newActorClass = {};
 
 newActorClass.tickEvent = function(id){
   //console.log("playerActor Got a tick :D");
-  console.log(self)
   self.location.x += 10*dt
 }
 
 newActorClass.EventPlay = function(id){
-  GetActorById(id).animation.image.src = "./content/sprites/TestAnim.png";
-  GetActorById(id).animation.buildAnimation("idle", 0, 2, true, 0.5);
-  GetActorById(id).animation.play("idle")
+  self.animation.image.src = "./content/sprites/TestAnim.png";
+  self.animation.buildAnimation("idle", 0, 2, true, 0.5);
+  self.animation.play("idle")
 }
--- project/engine/Structures/Vector2.js
+++ project/engine/Structures/Vector2.js
@@ -11,20 +11,24 @@ var Vector2 = function(x, y){
   this.x = x;
   this.y = y;
 };
+
 Vector2.prototype.Set = function (nx, ny){
   this.x = nx;
   this.y = ny;
 };
+
 Vector2.prototype.Magnitude = function (){
   var mag = this.x*this.x + this.y*this.y
   mag = Math.sqrt(mag);
   return mag;
 };
+
 Vector2.prototype.Normalize = function (){ //destructive
   var mag = this.Magnitude();
   this.x /= mag;
   this.y /= mag;
 };
+
 Vector2.prototype.GetNormal = function (){
   var mag = this.Magnitude();
   var v2 = new Vector2(0,0);
@@ -32,29 +36,47 @@ Vector2.prototype.GetNormal = function (){
   v2.y = this.y / mag;
   return v2;
 };
+
 Vector2.prototype.Add = function (other){
   this.x += other.x;
   this.y += other.y
 };
+
 Vector2.prototype.Multiply = function (scalar){
   this.x *= other.x;
   this.y *= other.y;
 };
-Vector2.prototype.RotateVector = function(center, end, angle){
-  var radians = (Math.PI / 180) * angle,
-		cos = Math.cos(radians),
-		sin = Math.sin(radians),
-		nx = (cos * (end[0] - center[0])) + (sin * (end[1] = center[1])) + center[0],
-		ny = (cos * (end[1] - center[1])) + (sin * (end[0] = center[0])) + center[1];
-	return [nx, ny];
+
+
+/*---------------------------------------------------
+  Function relevent to vector2
+---------------------------------------------------*/
+RotateVector(end, angle, center){
+  if (typeof(center) != "object"){
+    center = {};
+  }
+  if (typeof(center.x) != "number"){
+    center.x = 0;
+  }
+  if (typeof(center.y) != "number"){
+    center.y = 0;
+  }
+  var radians = (Math.PI / 180) * angle;
+	var cos = Math.cos(radians);
+	var sin = Math.sin(radians);
+	var nx = (cos * (end.x - center.x)) + (sin * (end.y + center.y)) + center.x;
+	var ny = (cos * (end.y - center.y)) + (sin * (end.x + center.x)) + center.x;
+	return {x:nx, y:ny};
 };
-Vector2.prototype.DistanceTo = function(end){
+
+DistanceTo(start, end){
   var NewEnd = [
-		(end.x - this.x),
-		(end.y - this.y)
+		(end.x - start.x),
+		(end.y - start.y)
 	];
   return Math.sqrt( (NewEnd[0] * NewEnd[0]) + (NewEnd[1] * NewEnd[1]) )
 };
-Vector2.prototype.LookAtRotation = function(target){
-  return (Math.atan((target.x-this.x), (target.y-this.y)) * 180 / Math.PI);
+
+LookAtRotation(start, target){
+  return (Math.atan((target.x-start.x), (target.y-start.y)) * 180 / Math.PI);
 }
