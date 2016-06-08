2a87d89335a9e4b146ad784cfb26154ca5bad0f4
RainFlavell <rain.flavell@gmail.com>
2016-06-08 19:54:25 +1000

    changed the animation of the car

--- project/content/actors/CarActor.js
+++ project/content/actors/CarActor.js
@@ -1,57 +1,25 @@
 var newActorClass = {};
 //SpawnActor({location: {x:0,y:0}, rotation: 0, size:{x:164,y:124}}, "playerActor")
-var ACCELERATION = 5;
-var CAR_TURN_SPEED = 5;
-
-
-var car = {
-    image: document.createElement("img"),
-    x: location.x,
-    y: location.y,
-    width: 93,
-    height: 80,
-    directionX: 0,
-    directionY: 0,
-    angularDirection: 0,
-    rotation: 0,
-};
-car.image.src = "content/sprites/car.png";
 
 newActorClass.tickEvent = function(dt, id){
   //console.log("playerActor Got a tick :D");
   //console.log(GetControllerById(GetActorById(id).controllerId).axis["MoveForward"])
   //console.log(GetControllerById(GetActorById(id).controllerId).axis["MoveRight"])
 
-console.log("forward:", GetControllerById(GetActorById(id).controllerId).axis["MoveForward"], " | side:",GetControllerById(GetActorById(id).controllerId).axis["MoveRight"] )
+  console.log("forward:", GetControllerById(GetActorById(id).controllerId).axis["MoveForward"], " | side:",GetControllerById(GetActorById(id).controllerId).axis["MoveRight"] )
 
 
+self.velocity.y += "MoveForward";
 
-
-
-  var s = Math.sin(car.rotation);
-  var c = Math.cos(car.rotation);
-
-  var xDir = (car.directionX * c) - (car.directionY * s);
-  var yDir = (car.directionX * s) + (car.directionY * c);
-
-  var xVel = xDir * ACCELERATION;
-  var yVel = yDir * ACCELERATION;
-
-  car.x += xVel;
-  car.y += yVel;
-  car.rotation += car.angularDirection * CAR_TURN_SPEED;
-  context.save();
-  context.translate(car.x, car.y);
-  context.rotate(car.rotation);
-  context.drawImage(
-      car.image, -car.width / 2, -car.height / 2);
-  context.restore();
 
 }
 
 newActorClass.EventPlay = function(id){
   //I am controlled by GetActorById(id).controlledActorID
   //Getting a controller input value  GetControllerById(GetActorById(id).controlledActorID).axis["MoveForward"]
+  self.animation.image.src = "./content/sprites/car.png";
+  GetActorById(id).animation.buildAnimation("idle", 0, 1, true, 0.5);
+  GetActorById(id).animation.play("idle")
   console.info("Spawned Actor: ", GetActorById(id));
 }
 
